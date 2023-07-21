565583e6888780523c5099dc6722467f04e15d80
vinoarystio <rocevo.id@gmail.com>
2023-07-21 09:19:10 +0700

    update

--- src/pages/gipSearch.tsx
+++ src/pages/gipSearch.tsx
@@ -10,37 +10,64 @@ export type Data = {
   title: string;
 };
 
+const defaultFormData = {
+  search: "",
+};
+
 const GipSearch = () => {
   const [data, setData] = useState<Data[] | null>();
+  const [formData, setFormData] = useState(defaultFormData);
+  const { search } = formData;
 
-  const getData = async (value: string) => {
+  const getData = async () => {
     const response = await axios.get(
-      `https://api.giphy.com/v1/gifs/search?api_key=SiWLd2c7lWedjOXrjYFWdq3ACGuNCxA3&q=${
-        value || "popular"
-      }&limit=9&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+      `https://api.giphy.com/v1/gifs/search?api_key=SiWLd2c7lWedjOXrjYFWdq3ACGuNCxA3&q=random&limit=9&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
     );
 
     const body = response.data.data;
     setData(body);
   };
 
-  const onChange = (e: { target: { value: string } }) => {
-    setTimeout(() => getData(e.target.value), 2000);
+  useEffect(() => {
+    console.log("Component Mounted");
+    getData();
+  }, []);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
   };
 
-  useEffect(() => {
-    getData("popular");
-  }, []);
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const response = await axios.get(
+      `https://api.giphy.com/v1/gifs/search?api_key=SiWLd2c7lWedjOXrjYFWdq3ACGuNCxA3&q=${
+        formData.search || "popular"
+      }&limit=9&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+    );
+
+    const body = response.data.data;
+    setData(body);
+
+    setFormData(defaultFormData);
+  };
 
   return (
     <section className="page-content">
       <div className="top">
         <h1>search your giphy</h1>
-        <input
-          type="text"
-          placeholder="Search Giphy"
-          onChange={(e) => onChange(e)}
-        />
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            id="search"
+            value={search}
+            placeholder="Search Giphy"
+            onChange={(e) => onChange(e)}
+          />
+        </form>
       </div>
       <div className="row">
         {data
@@ -51,6 +78,14 @@ const GipSearch = () => {
               </figure>
             ))
           : null}
+        {/* {dataUpdate
+          ? dataUpdate.map((item, index) => (
+              <figure key={index}>
+                <p>GIF</p>
+                <img src={item.images.original.url} alt={item.title} />
+              </figure>
+            ))
+          : null} */}
       </div>
     </section>
   );
